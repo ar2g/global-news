6cf6863ba79f8e8b7398ddb7e6239f427eb28a50
ar1g <ari.lounging+code@gmail.com>
2020-10-06 15:45:13 +0100

    component to retrieve data and display the top 5 stories

---
+++ src/components/TopStories.js
@@ -0,0 +1,50 @@
+import React, {Fragment, useEffect, useState} from 'react'
+
+export const TopStories = ({countryCode}) => {
+  
+  const API_URL = `https://newsapi.org/v2/top-headlines?country=${countryCode}`
+  
+  let headers = new Headers();
+  headers.append("x-api-key", "1198979c43af49d18fcdeffeeec571de")
+  
+  let requestOptions = {
+    method: 'GET',
+    headers,
+    redirect: 'follow'
+  }
+  
+  const [topStories, setTopStories] = useState(null);
+  
+  const getData = async () => {
+    
+    try {
+      const response = await fetch(API_URL, requestOptions)
+      const data = await response.json()
+      const topStories = data.articles.slice(0,5)
+      console.log(`topStories`, topStories)
+      setTopStories(topStories)
+
+    } catch(error) {
+      console.error(error)
+    }
+  } 
+
+  useEffect(() => {
+    getData()
+  }, [countryCode])
+
+  return (
+    <Fragment>
+        {topStories && topStories.map(i => {
+          return (
+            <Fragment key={`frag${i.url}`}>
+              {/* <img src={i.urlToImage} key={`img${i.urlToImage}`} /> */}
+              <p key={`p${i.url}`}>{i.title}</p>
+            </Fragment>
+          )
+        })}
+    </Fragment>
+  )
+  
+}
+
